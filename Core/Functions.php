b55d91a488344a7ea9ce40bf3009e28593219e7b
F0rest <95748287+F0rest-csgo@users.noreply.github.com>
2022-06-02 11:44:22 +0800

    Update QQ nickname api

--- Core/Functions.php
+++ Core/Functions.php
@@ -289,10 +289,10 @@ function load_ad()
 //获取QQ昵称
 function get_qq_nick($qq)
 {
-    $get_info = httpGet('https://api.unipay.qq.com/v1/r/1450000186/wechat_query?cmd=1&pf=mds_storeopen_qb-__mds_qqclub_tab_-html5&pfkey=pfkey&from_h5=1&from_https=1&openid=openid&openkey=openkey&session_id=hy_gameid&session_type=st_dummy&qq_appid=&offerId=1450000186&sandbox=&provide_uin=' . $qq);
-    $name = json_decode($get_info, true);
-    if (!isset($name['nick'])) return '获取失败！';
-    $name = urldecode($name['nick']);
+    $get_info = httpGet('https://api.usuuu.com/qq/' . $qq);
+    $name = json_decode($get_info,true);
+    if (!isset($name)) return '获取失败！';
+    $name = $name['data']['name'];
     return $name;
 }
 
